052aad49ea9a5b1ea80a68375f55e6944b25b6c5
RauuulP <raul.pacurar@accesa.eu>
2024-12-24 11:57:01 +0200

    tour-of-heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { HeroesComponent } from "./heroes/heroes.component";
+import { MessagesComponent } from './messages/messages.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, MessagesComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tour-of-heroes-app';
+  title = 'Tour of Heroes';
 }
